e978cf5fb6f8476ff671d0cf0ab6f4c76971287a
gonmeso <gmellizosoto@gmail.com>
2022-01-19 20:05:18 +0100

    Improve deployment and removal

--- 01_Static_Website/remove_site.sh
+++ 01_Static_Website/remove_site.sh
@@ -1,2 +1,4 @@
-aws s3 rm s3://bucket-name --recursive
-aws cloudformation delete-stack --stack-name static-site-with-cdn
+BUCKET=$(aws cloudformation describe-stacks --stack-name static-site-with-cdn --query "Stacks[0].Outputs[?OutputKey=='BucketURL'].OutputValue" --output text --profile udacity)
+aws s3 rm $BUCKET --recursive --profile udacity
+aws cloudformation delete-stack --stack-name static-site-with-cdn --profile udacity
+echo "Starting stack delete"
