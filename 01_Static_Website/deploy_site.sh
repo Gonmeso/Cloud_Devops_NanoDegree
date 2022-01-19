510b327e75b2c77656bcdc7a902f2f209055493c
gonmeso <gmellizosoto@gmail.com>
2022-01-19 21:45:54 +0100

    Add cloudfront deployment

--- 01_Static_Website/deploy_site.sh
+++ 01_Static_Website/deploy_site.sh
@@ -1,6 +1,8 @@
+set -xe
 aws cloudformation deploy --template-file ./static_site.yaml --stack-name static-site-with-cdn --profile udacity
 BUCKET=$(aws cloudformation describe-stacks --stack-name static-site-with-cdn --query "Stacks[0].Outputs[?OutputKey=='BucketURL'].OutputValue" --output text --profile udacity)
 cd files
 aws s3 cp --recursive  . $BUCKET --profile udacity
 echo "s3 URL: $(aws cloudformation describe-stacks --stack-name static-site-with-cdn --query "Stacks[0].Outputs[?OutputKey=='WebsiteURL'].OutputValue" --output text --profile udacity)"
 cd ..
+echo "Cloudfront URL: $(aws cloudformation describe-stacks --stack-name static-site-with-cdn --query "Stacks[0].Outputs[?OutputKey=='CFURL'].OutputValue" --output text --profile udacity)"
--- 01_Static_Website/remove_site.sh
+++ 01_Static_Website/remove_site.sh
@@ -1,4 +1,6 @@
+set -xe
 BUCKET=$(aws cloudformation describe-stacks --stack-name static-site-with-cdn --query "Stacks[0].Outputs[?OutputKey=='BucketURL'].OutputValue" --output text --profile udacity)
 aws s3 rm $BUCKET --recursive --profile udacity
 aws cloudformation delete-stack --stack-name static-site-with-cdn --profile udacity
+aws s3api delete-bucket --bucket $BUCKET --profile udacity
 echo "Starting stack delete"
