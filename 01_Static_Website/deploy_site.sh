c89072b0eabd5ff24f262e3a86917802aa1b05af
gonmeso <gmellizosoto@gmail.com>
2022-01-19 19:58:04 +0100

    improve deploy

--- 01_Static_Website/deploy_site.sh
+++ 01_Static_Website/deploy_site.sh
@@ -1,4 +1,6 @@
 aws cloudformation deploy --template-file ./static_site.yaml --stack-name static-site-with-cdn --profile udacity
+BUCKET=$(aws cloudformation describe-stacks --stack-name static-site-with-cdn --query "Stacks[0].Outputs[?OutputKey=='BucketURL'].OutputValue" --output text --profile udacity)
+echo "s3 URL: $(aws cloudformation describe-stacks --stack-name static-site-with-cdn --query "Stacks[0].Outputs[?OutputKey=='WebsiteURL'].OutputValue" --output text --profile udacity)"
 cd files
-aws s3 cp --recursive  . s3://static-site-with-cdn-s3bucket-1colg50kc5ywc --profile udacity
+aws s3 cp --recursive  . $BUCKET --profile udacity
 cd ..
