2ee63857d979f5bb60a6011fa738790b722a0d54
gonmeso <gmellizosoto@gmail.com>
2022-03-02 23:48:24 +0100

    Add conditional bastion host

--- 02_HA_WebApp_Using_CloudFormation/create-stack.sh
+++ 02_HA_WebApp_Using_CloudFormation/create-stack.sh
@@ -2,8 +2,8 @@
 
 # Create Network
 echo "Creating Network Stack"
-aws cloudformation deploy --stack-name network --template-file network/network-cf-template.yml --parameter-override file://network/network-parameters.json --capabilities CAPABILITY_IAM --region eu-west-1 --profile udacity
+aws cloudformation deploy --stack-name network --template-file network/network-cf-template.yml --parameter-override file://network/network-parameters.json --region eu-west-1 --profile udacity
 
 echo "Creating Servers Stack"
 # Create Servers
-aws cloudformation deploy --stack-name servers --template-file servers/server-cf-template.yml --capabilities CAPABILITY_IAM --region eu-west-1 --profile udacity
+aws cloudformation deploy --stack-name servers --template-file servers/server-cf-template.yml --parameter-override file://servers/server-parameters.json --capabilities CAPABILITY_IAM --region eu-west-1 --profile udacity
